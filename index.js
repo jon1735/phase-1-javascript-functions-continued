5955ebd8dfb3f02980a65260917870cc218701af
Jonathan Gonzalez <Jonathan.gonzalez@powerhrg.com>
2021-05-23 18:35:58 -0400

    added updated index.js module

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 // Your code here
+function saturdayFun(activity = 'roller-skate'){ 
+    return "This Saturday, I want to " + (activity)+"!"
+}
+const mondayWork = function(activity = 'go to the office'){
+    return `This Monday, I will ${activity}.`
+}
+
+function wrapAdjective(highlight = '*'){
+    return function(message = "special"){
+        return `You are ${highlight}${message}${highlight}!`
+    }
+}
+
